619edfd01a0c0f0003d5a011ba80541bbbf30b35
flemay <flemay@gmail.com>
2023-12-08 11:52:03 +0100

    Create array of data -- 15 artworks

---
+++ oeuvres.php
@@ -0,0 +1,94 @@
+<?php
+
+$oeuvres = [
+    [
+        'id' => '1',
+        'imageFile' => 'clark-van-der-beken',
+        'title' => 'Dodomu',
+        'description' => 'Mia Tozerski',
+    ],
+    [
+        'id' => '2',
+        'imageFile' => 'pawel-czerwinski-3',
+        'title' => 'Aashaaheen Baadal',
+        'description' => 'Anaisha Devi',
+    ],
+    [
+        'id' => '3',
+        'imageFile' => 'dan-cristian-padure',
+        'title' => 'Nightlife Traffic',
+        'description' => 'Andrew Forsythe',
+    ],
+    [
+        'id' => '4',
+        'imageFile' => 'steve-johnson-5',
+        'title' => "Le refuge de l'Havre",
+        'description' => 'Simon Pelletier',
+    ],
+    [
+        'id' => '5',
+        'imageFile' => 'steve-johnson',
+        'title' => 'Red Washover',
+        'description' => 'Kit Van Der Borght',
+    ],
+    [
+        'id' => '6',
+        'imageFile' => 'pawel-czerwinski',
+        'title' => 'Chromatics',
+        'description' => 'Jean-Michel Delatronchette',
+    ],
+    [
+        'id' => '7',
+        'imageFile' => 'jazmin-quaynor',
+        'title' => 'Digital Negative',
+        'description' => 'Hamish McKee',
+    ],
+    [
+        'id' => '8',
+        'imageFile' => 'steve-johnson-6',
+        'title' => 'Blast from the past',
+        'description' => 'Juliette Baskerville',
+    ],
+    [
+        'id' => '9',
+        'imageFile' => 'victor-grabarczyk',
+        'title' => 'Hurricane',
+        'description' => 'Natalie Wellington',
+    ],
+    [
+        'id' => '10',
+        'imageFile' => 'pawel-czerwinski-2',
+        'title' => 'La marée rouge',
+        'description' => 'Martin Rodriguez',
+    ],
+    [
+        'id' => '11',
+        'imageFile' => 'steve-johnson-2',
+        'title' => 'Asimilacion',
+        'description' => 'Angel Sanchez-Fernandez',
+    ],
+    [
+        'id' => '12',
+        'imageFile' => 'fly-d',
+        'title' => 'La Galaxia Gialla',
+        'description' => 'Eduardo Tancredi',
+    ],
+    [
+        'id' => '13',
+        'imageFile' => 'orfeas-green',
+        'title' => 'Puffy Amalgamate',
+        'description' => 'Sandro De Blasi',
+    ],
+    [
+        'id' => '14',
+        'imageFile' => 'steve-johnson-4',
+        'title' => 'Mirage',
+        'description' => 'Stéphanie Kaiser',
+    ],
+    [
+        'id' => '15',
+        'imageFile' => 'steve-johnson-3',
+        'title' => 'Blaue Gelbe Muster',
+        'description' => 'Adelheid Von Schreiber',
+    ],
+];
